4caf964582241f45b92f6f571b90826dad7da006
mcclains2003 <mcclains2003@gmail.com>
2017-12-26 12:10:34 -0600

    Removed List Items component... will discuss with instructor.

--- src/components/OilCard.js
+++ src/components/OilCard.js
@@ -3,16 +3,13 @@ import { Card, CardTitle, CardText,
  CardSubtitle, CardBody, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-import ListItems from './ListItems';
-
 export const OilCard = ({ oil, deleteButton }) => (
     <Card key={ oil.id }>
         <CardBody>
             <CardTitle>{ oil.name }</CardTitle>
             <CardText>{ oil.description }</CardText>
             <CardSubtitle>Uses:</CardSubtitle>
-                <ListItems uses={ oil.uses } />
-            {/* <CardText>{ oil.uses }</CardText> */}
+            <CardText>{ oil.uses }</CardText>
             <CardSubtitle>Scent Profile:</CardSubtitle>
             <CardText>{ oil.fragrance_profile }</CardText>
             <CardSubtitle>Medical Properties:</CardSubtitle>
