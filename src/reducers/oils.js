338a1ae6cd0f9dbdf466515cfd525f981871902a
mcclains2003 <mcclains2003@gmail.com>
2017-12-29 11:46:45 -0600

    Added a sort function to the reducer to always return oils in alpha order by name

--- src/reducers/oils.js
+++ src/reducers/oils.js
@@ -1,7 +1,9 @@
 export default (state = [], action) => {
     switch(action.type) {
         case 'GET_OILS_SUCCESS':
-            return action.oils;
+            let sortedOils = action.oils.sort(function(a, b){ return a.name > b.name; });
+            
+            return sortedOils;
         case 'CREATE_OIL_SUCCESS':
             return state.concat(action.oil);
         case 'DELETE_OIL_SUCCESS':
